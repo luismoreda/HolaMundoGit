13836be84dd388735fa1e624911110cbebc67fe5
Luis Moreda <luismoredags@gmail.com>
2024-02-12 15:38:25 +0100

    2 cambio Consola

--- src/Mundo/HolaMundo.java
+++ src/Mundo/HolaMundo.java
@@ -5,7 +5,11 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		// TODO Esbozo de método generado automáticamente
 		
-		System.out.println("l cambio - eclipse");
+                System.out.println("1 cambio - consola");
+		System.out.println("1 cambio - eclipse");
+		System.out.println("2 cambio - eclipse");
+                System.out.println("2 cambio - eclipse");
+		
 	}
 
 }
