3c1d74c55122be2ee1955f59b9b1d5291bf6cbe9
luism <luismoredags@gmail.com>
2024-02-12 13:24:58 +0100

    l cambio eclipse

--- src/Mundo/HolaMundo.java
+++ src/Mundo/HolaMundo.java
@@ -5,7 +5,7 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		// TODO Esbozo de método generado automáticamente
 		
-		System.out.println("Busando la luz");
+		System.out.println("l cambio - eclipse");
 	}
 
 }
